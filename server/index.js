a75e5887a0bdf7a9425ae158da48421404d207c4
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-03-13 14:25:10 -0400

    spelling

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const { client } = require('./db');
 
-//building the init
+//building the init functionality
 const init = async () => {
   console.log('connecting to db');
   await client.connect();
