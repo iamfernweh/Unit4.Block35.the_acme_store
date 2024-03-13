a1fc408caea22459e14aee333552ef667aa55615
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-03-13 14:32:34 -0400

    products table

--- server/db.js
+++ server/db.js
@@ -15,7 +15,10 @@ const createTables = async () => {
         username VARCHAR(20) UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL
     );
-    CREATE TABLE products();
+    CREATE TABLE products(
+        id UUID PRIMARY KEY,
+        name VARCHAR (128) UNIQUE NOT NULL
+    );
     CREATE TABLE favorites();
 
   `;
