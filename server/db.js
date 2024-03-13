8d819ad56eb503384caa8ef8e9775b0673755431
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-03-13 15:39:49 -0400

    fixed spelling

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const pg = require('pg');
 const client = new pg.Client(
-  process.env.DATABASE_URL || 'posgres://localhost/acme_store_db'
+  process.env.DATABASE_URL || 'postgres://localhost/acme_store_db'
 );
 const uuid = require('uuid');
 const bcrypt = require('bcrypt');
