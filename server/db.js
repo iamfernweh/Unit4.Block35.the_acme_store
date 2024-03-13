4598d36c1ee6cf6deb567bcbc8d42bfbc88b8ad2
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-03-13 14:28:18 -0400

    starting create tables

--- server/db.js
+++ server/db.js
@@ -2,4 +2,12 @@ const pg = require('pg');
 const client = new pg.Client(
   process.env.DATABASE_URL || 'posgres://localhost/acme_store_db'
 );
-module.exports = { client };
+
+//create the table
+const createTables = async () => {
+  const SQL = ``;
+  await client.query(SQL);
+};
+
+//always remember to export
+module.exports = { client, createTables };
--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
-const { client } = require('./db');
+const { client, createTables } = require('./db');
 
 //building the init functionality
 const init = async () => {
   console.log('connecting to db');
   await client.connect();
   console.log('connected to db');
+  await createTables();
+  console.log('tables created');
 };
 
 init();
