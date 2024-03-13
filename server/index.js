24f9a3e553ddb4bedf7a96b05f9820ed3929caaa
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-03-13 14:24:37 -0400

    completed client setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const { client } = require('./db');
 
 //building the init
-const init = async () => {};
+const init = async () => {
+  console.log('connecting to db');
+  await client.connect();
+  console.log('connected to db');
+};
 
 init();
