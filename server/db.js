78f8af672401487c54c2b13679575988d27fe664
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-03-13 14:31:33 -0400

    table users

--- server/db.js
+++ server/db.js
@@ -2,10 +2,23 @@ const pg = require('pg');
 const client = new pg.Client(
   process.env.DATABASE_URL || 'posgres://localhost/acme_store_db'
 );
+const uuid = require('uuid');
 
 //create the table
 const createTables = async () => {
-  const SQL = ``;
+  const SQL = `
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS products;
+    DROP TABLE IF EXISTS favorites;
+    CREATE TABLE users(
+        id UUID PRIMARY KEY,
+        username VARCHAR(20) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL
+    );
+    CREATE TABLE products();
+    CREATE TABLE favorites();
+
+  `;
   await client.query(SQL);
 };
 
